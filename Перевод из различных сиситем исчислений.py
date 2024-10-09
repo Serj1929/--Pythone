b66370efb89da959215901a6f07579b0afee0bda
Serj1929 <145355358+Serj1929@users.noreply.github.com>
2024-10-09 19:30:34 +0300

    Update Перевод из различных сиситем исчислений.py

--- Перевод из различных сиситем исчислений.py
+++ Перевод из различных сиситем исчислений.py
@@ -1,11 +1,11 @@
 numbers = [888,300,356,456,7890]
 
-bin_numbers= [bin(n)[2:] for n in numbers
-oct_numbers= [oct(n)[2:] for n in numbers 
-hex_numbers= [hex(n)[2:] for n in numbers 
-
-              print(bin_numbers)
+bin_numbers= [bin(n)[2:] for n in numbers]
+oct_numbers= [oct(n)[2:] for n in numbers]
+hex_numbers= [hex(n)[2:] for n in numbers]
 print()
-              print(oct_numbers)
+print(bin_numbers)
 print()
-              print(hex_numbers)
+print(oct_numbers)
+print()
+print(hex_numbers)
